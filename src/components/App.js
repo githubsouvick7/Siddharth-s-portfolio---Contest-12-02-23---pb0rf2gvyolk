303772993003d47e47ae21adbdedb61e6c71ce48
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-02-12 13:48:39 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,17 @@ const App = () => {
 
   return (
     <div id="main">
-
-
       <Router>
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
-          <route path='/blog' exact>
+          <Route path='/blog'>
             <Blog />
-          </route>
-          <route path='/info' exact>
+          </Route>
+          <Route path='/Info'>
             <Info />
-          </route>
+          </Route>
         </Switch>
       </Router>
     </div>
